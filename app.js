3d58b3ca728a7002cdd004b24b0c92a2db959654
Ian King <ianking741@gmail.com>
2021-07-09 08:27:12 -0500

    Update port number and modified middleware.

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-const koa = require('koa');
-const app = new koa();
+// Koa Instantiation
+const Koa = require('koa');
+const app = new Koa();
 
+const http = require('http');
 const helmet = require('koa-helmet');
+const bodyParser = require('koa-bodyparser');
+const json = require('koa-json');
 const render = require('koa-ejs');
 const { join } = require('path');
+const router = require('./routes/routes');
+const { tryify } = require('./utils/klar');
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const router = require('./routes/routes');
-
 const dbURI = process.env.dbURI;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+
+
 
 // Security Headers Middleware
 app.use(helmet());
 
-// Add additional properties to context
-app.context.user = 'Ian';
+// Koa Body Parser
+app.use(bodyParser());
 
+// JSON Prettier Middleware
+app.use(json());
+
+// Koa EJS Configuration
 render(app, {
   root: join(__dirname, 'views'),
   layout: 'layout',
@@ -26,6 +37,43 @@ render(app, {
 });
 
 // Router Middleware
-app.use(router.routes()).use(router.allowedMethods());
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-app.listen(port, () => console.log(`Server running on port: ${port}`));
+
+
+/**
+ * Database connection function.
+ * @param {string} URI - Mongo Atlas URI string.
+ * @param {number} PORT - Port number.
+ */
+const client = new MongoClient(dbURI, {
+  poolSize: 5,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+(async () => {
+  const [data, error] = await tryify(client.connect());
+  if (error) { throw new Error(error) };
+})()
+
+const Server = http.createServer(app.callback()).listen(port, () => console.log('Server up and running...'));
+
+
+const cleanup = () => {
+  console.log(Server);
+  console.log(Server.close(() => { console.log('closed')}));
+  client.close();
+  console.log('\nServer closed...');
+  console.log('\nMongo connection closed...');
+}
+
+process.on('SIGTERM', () => {
+  cleanup();
+});
+
+process.on('SIGINT', () => {
+  console.log(Server);
+  cleanup()
+});
